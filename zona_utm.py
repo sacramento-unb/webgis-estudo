6121a53c57537a96daf813524cfc1f7f7bc0000b
sacramento.unb <sacramento.unb@gmail.com>
2025-02-27 02:03:51 -0300

    Corrige a pasta de dados

--- zona_utm.py
+++ zona_utm.py
@@ -1,12 +1,15 @@
-import math
 import geopandas as gpd
+import math
 
 def calcular_utm(gdf):
     gdf = gdf.dissolve(by=None)
     centroid = gdf.centroid.iloc[0]
-    zona = math.ceil((centroid.x+180)/6)
+    zona = (math.floor((centroid.x+180)/6))+1
     if centroid.y > 0:
         epsg = zona + 32600
     else:
         epsg = zona + 32700
     return epsg
+
+if __name__ == '__main__':
+    calcular_utm(gpd.read_file('/dados/car.geojson'))
